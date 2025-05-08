d22537ff41d52252b14b6df194b3ed7301995dc6
Hà Nguyễn Thị Thu <102210032@sv1.dut.udn.vn>
2025-05-08 11:17:08 +0700

    fix upload again

--- src/app/(authenticated)/spaces/[id]/chat/components/chat-interface.tsx
+++ src/app/(authenticated)/spaces/[id]/chat/components/chat-interface.tsx
@@ -166,7 +166,7 @@ export default function ChatInterface() {
   const displayMessages = tempMessage ? [...messages, tempMessage] : messages;
 
   return (
-    <div className="flex w-full">
+    <div key={sessionId} className="flex w-full">
       <div className="flex flex-col mx-5 mt-5 flex-1 bg-gradient-to-b from-background to-background/95 rounded">
         <ChatHeader onClearChat={clearChat} />
 
