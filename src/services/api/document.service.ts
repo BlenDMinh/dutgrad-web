1f68b0b116ceb5c626fa7bba7e11e67252a648d6
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2025-05-10 09:40:04 +0700

    Passing additional header for Mime-Type

--- src/services/api/document.service.ts
+++ src/services/api/document.service.ts
@@ -4,26 +4,36 @@ import { handleResponse } from "./helper";
 import { AxiosProgressEvent } from "axios";
 
 export const documentService = {
-    uploadDocumet(spaceId: number, file: File, onUploadProgress?: (progressEvent: AxiosProgressEvent) => void) {
-        const formData = new FormData();
-        formData.append("space_id", spaceId.toString()); 
-        formData.append("file", file);
+  uploadDocumet(
+    spaceId: number,
+    file: File,
+    onUploadProgress?: (progressEvent: AxiosProgressEvent) => void
+  ) {
+    const formData = new FormData();
+    formData.append("space_id", spaceId.toString());
+    formData.append("file", file);
+    const mimeType = file.type;
 
-        return apiClient.instance.post(API_ROUTES.DOCUMENT.UPLOAD, formData, {
-            headers: {
-                "Content-Type": "multipart/form-data",
-                "Accept": "application/json"
-            },
-            onUploadProgress: onUploadProgress,
-            timeout: 0
-        });
-    },
-    getDocumentById: async(documentId: number) => {
-        const response = await apiClient.instance.get(API_ROUTES.DOCUMENT.DETAIL(documentId.toString()));
-        return handleResponse(response.data);
-    },
-    deleteDocument: async(documentId: string)=> {
-        const response = await apiClient.instance.delete(API_ROUTES.DOCUMENT.DELETE(documentId));
-        return handleResponse(response.data);
-    }
+    return apiClient.instance.post(API_ROUTES.DOCUMENT.UPLOAD, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Accept: "application/json",
+        "Mime-Type": mimeType,
+      },
+      onUploadProgress: onUploadProgress,
+      timeout: 0,
+    });
+  },
+  getDocumentById: async (documentId: number) => {
+    const response = await apiClient.instance.get(
+      API_ROUTES.DOCUMENT.DETAIL(documentId.toString())
+    );
+    return handleResponse(response.data);
+  },
+  deleteDocument: async (documentId: string) => {
+    const response = await apiClient.instance.delete(
+      API_ROUTES.DOCUMENT.DELETE(documentId)
+    );
+    return handleResponse(response.data);
+  },
 };
